252a8b939c9dec2744a53d8503ce3f35eeacb557
alvarosh15 <alvaro.s.h.15@gmail.com>
2024-02-25 17:48:45 +0100

    changes in editor

--- src/app/components/Editor.js
+++ src/app/components/Editor.js
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { parseMarkdown } from "minimarkdown";
 import "./style.css";
 
